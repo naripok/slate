a87a9a8d14a58c726723d4c57b09b9c7636fedc3
naripok <fernando.canteruccio@gmail.com>
2020-09-14 12:06:19 -0300

    fix iframe example path and discorery extension

--- site/pages/api/index.ts
+++ site/pages/api/index.ts
@@ -6,6 +6,6 @@ const examplePath = join(process.cwd(), 'examples')
 export function getAllExamples() {
   const slugs = readdirSync(examplePath)
   return slugs
-    .filter(name => name.match(/.tsx$/))
-    .map(n => n.replace(/.tsx$/, ''))
+    .filter(name => name.match(/.(tsx|js)$/))
+    .map(n => n.replace(/.(tsx|js)$/, ''))
 }
